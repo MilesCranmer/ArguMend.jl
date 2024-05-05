69d3ff488df89859996932af3134969c1262310a
MilesCranmer <miles.cranmer@gmail.com>
2024-05-06 00:33:45 +0100

    add other utility to get all matching subsequences between sequences

--- src/ArguMend.jl
+++ src/ArguMend.jl
@@ -75,11 +75,56 @@ end
 
 """Find longest matching sequence between a and b"""
 function longest_match(a, b)
+    matches = all_matching_subsequences(a, b)
+    max_len = maximum(m -> m.len, matches; init=0)
+    if max_len == 0
+        return Match(a_start=firstindex(a), b_start=firstindex(b), len=0)
+    else
+        return first(filter(m -> m.len == max_len, matches))
+    end
+end
+
+@testitem "Test longest match" begin
+    using ArguMend: longest_match, Match
+
+    @test longest_match("abc", "bcd") == Match(a_start=2, b_start=1, len=2)
+
+    # Prefers the first match:
+    @test longest_match("1234", "12 34") == Match(a_start=1, b_start=1, len=2)
+
+    # No match will have len 0
+    @test longest_match("1", "2") == Match(a_start=1, b_start=1, len=0)
+
+    # Works for other collections
+    @test longest_match([1, 2, 3], [2, 3, 4, 5, 6, 1, 2, 3]) ==
+          Match(a_start=1, b_start=6, len=3)
+end
+
+"""
+Return a vector of all matching subsequences
+
+This will skip subsequences which are completely
+contained in a larger subsequence, and offset
+at the same distance.
+"""
+function all_matching_subsequences(a, b)
     # Most kwargs are pretty short, and this will be evaluated
     # only in the scope of bad function signatures,
     # so we can just brute force it.
-    match = Match(a_start = firstindex(a), b_start = firstindex(b), len = 0)
+    matches = Match[]
     for a_start in eachindex(a), b_start in eachindex(b)
+        already_included = any(matches) do m
+            # First, check if same offset from start
+            # a previous sequence, and contained within the
+            # matching length
+            within_a = a_start - m.a_start < m.len
+            within_b = b_start - m.b_start < m.len
+            same_distance = (a_start - m.a_start) == (b_start - m.b_start)
+            return within_a && within_b && same_distance
+        end
+        if already_included
+            continue
+        end
         len = 0
         a_i = a_start
         b_i = b_start
@@ -88,27 +133,50 @@ function longest_match(a, b)
             a_i = nextind(a, a_i)
             b_i = nextind(b, b_i)
         end
-        if len > match.len
-            match = Match(; a_start, b_start, len)
+        if len > 0
+            push!(matches, Match(; a_start, b_start, len))
         end
     end
-    return match
+    return matches
 end
 
-@testitem "Test longest match" begin
-    using ArguMend: longest_match, Match
+@testitem "Test all matching subsequences" begin
+    using ArguMend: all_matching_subsequences, Match
 
-    @test longest_match("abc", "bcd") == Match(a_start = 2, b_start = 1, len = 2)
+    @test all_matching_subsequences("abc", "abc") == [Match(a_start=1, b_start=1, len=3)]
+    @test all_matching_subsequences([1, 2, 3], [1, 2, 3]) == [Match(a_start=1, b_start=1, len=3)]
+    @test all_matching_subsequences("abc", "abababc") == [
+        Match(a_start=1, b_start=1, len=2),
+        Match(a_start=1, b_start=3, len=2),
+        Match(a_start=1, b_start=5, len=3),
+    ]
 
-    # Prefers the first match:
-    @test longest_match("1234", "12 34") == Match(a_start = 1, b_start = 1, len = 2)
+    # No Matches
+    @test isempty(all_matching_subsequences("abc", "def"))
+    @test isempty(all_matching_subsequences([1, 2, 3], [4, 5, 6]))
 
-    # No match will have len 0
-    @test longest_match("1", "2") == Match(a_start = 1, b_start = 1, len = 0)
+    # Overlapping matches
+    @test all_matching_subsequences("aaaa", "aa") == [
+        Match(a_start=1, b_start=1, len=2),
+        Match(a_start=1, b_start=2, len=1),
+        Match(a_start=2, b_start=1, len=2),
+        Match(a_start=3, b_start=1, len=2),
+        Match(a_start=4, b_start=1, len=1),
+    ]
+    @test all_matching_subsequences([1, 2, 1, 2], [1, 2]) == [
+        Match(a_start=1, b_start=1, len=2),
+        Match(a_start=3, b_start=1, len=2),
+    ]
 
-    # Works for other collections
-    @test longest_match([1, 2, 3], [2, 3, 4, 5, 6, 1, 2, 3]) ==
-          Match(a_start = 1, b_start = 6, len = 3)
+    # Edge Cases
+    @test isempty(all_matching_subsequences("", "abc"))
+    @test isempty(all_matching_subsequences("abc", ""))
+    @test all_matching_subsequences("a", "a") == [Match(a_start=1, b_start=1, len=1)]
+end
+
+
+
+function similarity_ratio(a, b)
 end
 
 end
